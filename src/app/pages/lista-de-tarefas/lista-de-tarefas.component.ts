541b8c95ead1a770cafc4501dc87cbfb5b065051
Lorenzo Fernandes Aguiar <lorenzofer00@gmail.com>
2025-04-29 15:32:14 -0300

    Adiciona o componente ListaDeTarefasComponent
    Foram adicionados os arquivos lista-de-tarefas.component.ts, lista-de-tarefas.component.spec.ts, lista-de-tarefas.component.html e lista-de-tarefas.component.css. Esses arquivos estão relacionados com o componente ListaDeTarefasComponent.

---
+++ src/app/pages/lista-de-tarefas/lista-de-tarefas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-tarefas',
+  imports: [],
+  templateUrl: './lista-de-tarefas.component.html',
+  styleUrl: './lista-de-tarefas.component.css'
+})
+export class ListaDeTarefasComponent {
+
+}
