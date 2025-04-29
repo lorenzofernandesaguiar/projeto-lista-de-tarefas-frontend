76f3bc131d9b4d3009b631e840e0aa43810c7d19
Lorenzo Fernandes Aguiar <lorenzofer00@gmail.com>
2025-04-29 15:38:04 -0300

    Adiciona o componente AdicionarTarefaComponent
    Foram adicionados os arquivos adicionar-tarefa.component.ts, adicionar-tarefa.component.spec.ts, adicionar-tarefa.component.html e adicionar-tarefa.component.css. Esses arquivos estão relacionados com o componente AdicionarTarefaComponent.

---
+++ src/app/pages/adicionar-tarefa/adicionar-tarefa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar-tarefa',
+  imports: [],
+  templateUrl: './adicionar-tarefa.component.html',
+  styleUrl: './adicionar-tarefa.component.css'
+})
+export class AdicionarTarefaComponent {
+
+}
