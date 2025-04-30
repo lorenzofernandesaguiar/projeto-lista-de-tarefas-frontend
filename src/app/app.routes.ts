121666031571c834aa1a2018a23edf307a87ea47
Lorenzo Fernandes Aguiar <lorenzofer00@gmail.com>
2025-04-30 10:18:53 -0300

    Altera o arquivo app.routes.ts
    O arquivo app.routes.ts recebeu alterações. Essas alterações consistiram na definição das rotas do front-end da aplicação web.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { ListaDeTarefasComponent } from './pages/lista-de-tarefas/lista-de-tarefas.component';
+import { AdicionarTarefaComponent } from './pages/adicionar-tarefa/adicionar-tarefa.component';
+import { EditarTarefaComponent } from './pages/editar-tarefa/editar-tarefa.component';
+import { ExcluirTarefaComponent } from './pages/excluir-tarefa/excluir-tarefa.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListaDeTarefasComponent,
+        title: 'Lista de tarefas'
+    },
+    {
+        path: 'adicionar-tarefa',
+        component: AdicionarTarefaComponent,
+        title: 'Adicionar tarefa'
+    },
+    {
+        path: 'editar/:id',
+        component: EditarTarefaComponent,
+        title: 'Editar tarefa',
+    },
+    {
+        path: 'excluir/:id',
+        component: ExcluirTarefaComponent,
+        title: 'Excluir tarefa',
+    }
+];
