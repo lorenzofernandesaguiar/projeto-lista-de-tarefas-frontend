57eb26479306af1f5da9db0cc498cc056ebe1193
Lorenzo Fernandes Aguiar <lorenzofer00@gmail.com>
2025-04-30 10:13:43 -0300

    Adiciona o componente ExcluirTarefaComponent
    Foram adicionados os arquivos excluir-tarefa.component.ts, excluir-tarefa.component.spec.ts, excluir-tarefa.component.html e excluir-tarefa.component.css. Esses arquivos estão relacionados com o componente ExcluirTarefaComponent.

---
+++ src/app/pages/excluir-tarefa/excluir-tarefa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-excluir-tarefa',
+  imports: [],
+  templateUrl: './excluir-tarefa.component.html',
+  styleUrl: './excluir-tarefa.component.css'
+})
+export class ExcluirTarefaComponent {
+
+}
