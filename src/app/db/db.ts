c550bf90b4caf722aaff16ae5c6156f340eef208
Lorenzo Fernandes Aguiar <lorenzofer00@gmail.com>
2025-04-30 11:49:32 -0300

    Adiciona o arquivo db.ts
    O arquivo db.ts foi adicionado. O arquivo db.ts recebeu alterações. Por causa dessas alterações, o arquivo db.ts passou a simular uma lista de tarefas armazenada em um banco de dados.

---
+++ src/app/db/db.ts
@@ -0,0 +1,33 @@
+import { Tarefa } from "../models/tarefa";
+
+export const mockListaDeTarefas: Tarefa[] = [
+    {
+        id: 1,
+        descricao: "Agendar reunião",
+        concluida: false
+    },
+
+    {
+        id: 2,
+        descricao: "Ler um livro",
+        concluida: true
+    },
+
+    {
+        id: 3,
+        descricao: "Concluir relatório",
+        concluida: false
+    },
+
+    {
+        id: 4,
+        descricao: "Comprar presente",
+        concluida: false
+    },
+
+    {
+        id: 5,
+        descricao: "Pagar as contas",
+        concluida: false
+    }
+];
