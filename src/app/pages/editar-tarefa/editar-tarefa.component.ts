742f1ca86bb1983e82c3c6903be743766ed0062a
Lorenzo Fernandes Aguiar <lorenzofer00@gmail.com>
2025-04-30 10:07:34 -0300

    Adiciona o componente EditarTarefaComponent
    Foram adicionados os arquivos editar-tarefa.component.ts, editar-tarefa.component.spec.ts, editar-tarefa.component.html e editar-tarefa.component.css. Esses arquivos estão relacionados com o componente EditarTarefaComponent.

---
+++ src/app/pages/editar-tarefa/editar-tarefa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-tarefa',
+  imports: [],
+  templateUrl: './editar-tarefa.component.html',
+  styleUrl: './editar-tarefa.component.css'
+})
+export class EditarTarefaComponent {
+
+}
