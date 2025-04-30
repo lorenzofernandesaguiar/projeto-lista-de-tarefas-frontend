4c1b6dfe27e7a86f77a886231d983aa7eed2f5b4
Lorenzo Fernandes Aguiar <lorenzofer00@gmail.com>
2025-04-30 11:28:50 -0300

    Altera o arquivo excluir-tarefa.component.ts
    O arquivo excluir-tarefa.component.ts recebeu alterações. Sobre essas alterações, pode-se destacar:
    
    - A inclusão de um método capaz de chamar o método do serviço TarefaService que é responsável por buscar uma tarefa específica.
    - A inclusão de um método capaz de chamar o método do serviço TarefaService que é responsável por excluir uma tarefa.

--- src/app/pages/excluir-tarefa/excluir-tarefa.component.ts
+++ src/app/pages/excluir-tarefa/excluir-tarefa.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Tarefa } from '../../models/tarefa';
+import { TarefaService } from '../../services/tarefa.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-excluir-tarefa',
@@ -7,5 +10,46 @@ import { Component } from '@angular/core';
   styleUrl: './excluir-tarefa.component.css'
 })
 export class ExcluirTarefaComponent {
+
+  public tarefa: Tarefa = new Tarefa(0, "", false);
+  public tarefaService: TarefaService = inject(TarefaService);
+  public router: Router = inject(Router);
+  public activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+
+  constructor() {
+
+    // Guarda na constante id o trecho da URL que corresponde ao id da tarefa.
+    const id: number = parseInt(this.activatedRoute.snapshot.params['id'], 10);
+
+    // Chama o método buscarTarefaPeloId do componente ExcluirTarefaComponent. A constante id é
+    // passada como parâmetro para esse método
+    this.buscarTarefaPeloId(id);
+
+  }
+
+  public buscarTarefaPeloId(id: number) {
+
+    // Chama o método buscarTarefaPeloId do serviço TarefaService
+    this.tarefaService.buscarTarefaPeloId(id).subscribe((tarefaBuscada: Tarefa) => {
+
+      // O retorno é guardado na propriedade tarefa pertencente ao componente ExcluirTarefaComponent
+      this.tarefa = tarefaBuscada;
+
+    });
+
+  }
+
+  public excluirTarefa() {
+
+    // Chama o método excluirTarefa do serviço TarefaService, passando como parâmetro a propriedade
+    // tarefa pertencente ao componente ExluirTarefaComponent
+    this.tarefaService.excluirTarefa(this.tarefa.id).subscribe(() => {
+
+      // Depois de excluir a tarefa, navega para a página principal
+      this.router.navigate(['']);
+
+    });
+
+  }
 
 }
