d9c40520e821280b8b68fe5aab10b05277e3f309
Lorenzo Fernandes Aguiar <lorenzofer00@gmail.com>
2025-04-30 11:06:14 -0300

    Altera o arquivo adicionar-tarefa.component.ts
    O arquivo adicionar-tarefa.component.ts recebeu alterações. Sobre essas alterações, pode-se destacar:
    
    - A importação da classe FormsModule para o template do componente AdicionarTarefaComponent.
    - A inclusão de um método capaz de chamar o método do serviço TarefaService que é responsável por adicionar uma tarefa.

--- src/app/pages/adicionar-tarefa/adicionar-tarefa.component.ts
+++ src/app/pages/adicionar-tarefa/adicionar-tarefa.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Tarefa } from '../../models/tarefa';
+import { TarefaService } from '../../services/tarefa.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adicionar-tarefa',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './adicionar-tarefa.component.html',
   styleUrl: './adicionar-tarefa.component.css'
 })
 export class AdicionarTarefaComponent {
+
+  public tarefa: Tarefa = new Tarefa(0, "", false);
+  public tarefaService: TarefaService = inject(TarefaService);
+  public router: Router = inject(Router);
+
+  public adicionarTarefa() {
+
+    // Chama o método adicionarTarefa do serviço TarefaService, passando como parâmetro a
+    // propriedade tarefa pertencente ao componente AdicionarTarefaComponent.
+    this.tarefaService.adicionarTarefa(this.tarefa).subscribe(() => {
+
+      // Depois de salvar a tarefa no banco de dados, navega para a página principal
+      this.router.navigate(['']);
+
+    });
+
+  }
 
 }
