2ea206a2e305c7bfdd0f6e312850d847bb45b555
Lorenzo Fernandes Aguiar <lorenzofer00@gmail.com>
2025-04-30 11:45:56 -0300

    Altera o arquivo tarefa.spec.ts
    O arquivo tarefa.spec.ts recebeu alterações. Sobre essas alterações, pode-se destacar:
    
    - A remoção do teste que vem por padrão.
    - A adição de um novo teste.

--- src/app/models/tarefa.spec.ts
+++ src/app/models/tarefa.spec.ts
@@ -1,7 +1,12 @@
 import { Tarefa } from './tarefa';
 
 describe('Tarefa', () => {
-  it('should create an instance', () => {
-    expect(new Tarefa()).toBeTruthy();
+
+  it('deve verificar se é criada uma instância da classe Tarefa', () => {
+
+    // Chama o constructor da classe Tarefa. Depois, verifica se o retorno não é undefined
+    expect(new Tarefa(0, "", false)).toBeTruthy();
+
   });
+  
 });
